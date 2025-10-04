332101a99b1b3b5345a771d55ec80ced4faec97a
Marcin <242138+mrcne@users.noreply.github.com>
2025-10-04 14:39:16 +0200

    feat(apps:web): typography improvements

--- apps/web/app/investigate/page.tsx
+++ apps/web/app/investigate/page.tsx
@@ -1,11 +1,10 @@
 import FeaturesForm from "@/components/containers/FeaturesForm";
+import { H1 } from "@/components/ui/typography";
 
 export default function Investigate() {
   return (
     <div className="max-w-xl mx-auto p-6">
-      <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-        Investigate
-      </h1>
+      <H1>Investigate</H1>
       <FeaturesForm />
     </div>
   );
