612cbe5985c5ca6fe2512f8cabbe85ae03d28f50
Marcin <242138+mrcne@users.noreply.github.com>
2025-10-05 17:50:12 +0200

    feat(apps:web): investigate - skip count

--- apps/web/components/components/ObjectsTable.tsx
+++ apps/web/components/components/ObjectsTable.tsx
@@ -206,30 +206,6 @@ const ObjectsTable: React.FC<Props> = ({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
     </div>
   )
 }
