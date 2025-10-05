315cabbe18b8fa55c2d88a20624cf2b7a2e145f8
Marcin <242138+mrcne@users.noreply.github.com>
2025-10-05 23:20:46 +0200

    feat(apps:web): homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,23 @@
+import { H1 } from "@/components/ui/typography";
+import Image from "next/image";
+import capExoplanet from "../assets/cap-exoplanet.webp";
+
 export default function Home() {
   return (
     <div>
-      <main>
-        <h1>Captain Exoplanet</h1>
+      <main className="text-center">
+        <H1>Captain Exoplanet</H1>
+        <div className="mt-4 flex justify-center">
+          <Image
+            src={capExoplanet}
+            alt="Captain Exoplanet illustration"
+            placeholder="empty"
+            priority
+            sizes="50vw"
+            className="w-[50vw] h-auto"
+          />
+        </div>
+        <p>Welcome traveller! The goal is of this app is to let anyone open a browser, paste or upload feature values, and get a classification back.</p>
       </main>
     </div>
   );
